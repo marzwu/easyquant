76d5ce5752bca1c0fef899c6777202e082c09af9
marzwu <marzwu@gmail.com>
2016-03-15 08:35:00 +0800

    增加开市判断

--- strategies/PB.py
+++ strategies/PB.py
@@ -1,13 +1,34 @@
 from easyquant import StrategyTemplate
+from easyquant import DefaultLogHandler
 
 
 class Strategy(StrategyTemplate):
     name = 'PB'
+    is_open = False
 
     def strategy(self, event):
-        self.log.info('\n\n策略2触发')
+        if not self.is_open:
+            pass
+
+        self.log.info('\n\n策略PB触发')
         self.log.info('行情数据: 华宝油气 %s' % event.data['162411'])
         self.log.info('检查持仓')
         self.log.info(self.user.balance)
         self.log.info('\n')
+
+    def check(self, event):
+        if event.data.clock_event == 'open':
+            # 开市了
+            self.log.info('open')
+            self.is_open = True
+        elif event.data.clock_event == 'close':
+            # 收市了
+            self.log.info('close')
+            self.is_open = False
+        elif event.data.clock_event == 5:
+            # 5 分钟的 clock
+            self.log.info("5分钟")
+
+    def log_handler(self):
+        return DefaultLogHandler(self.name, log_type='file', filepath='pb.log')
 
