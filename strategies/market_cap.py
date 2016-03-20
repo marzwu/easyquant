4b11312a9e2a7961d7dee53ccf339d3149aa9478
marzwu <marzwu@gmail.com>
2016-03-20 23:30:34 +0800

    小市值买卖逻辑

--- strategies/market_cap.py
+++ strategies/market_cap.py
@@ -12,14 +12,18 @@ class Strategy(StrategyTemplate):
     is_open = False
     last_sort_date = None
     min_cap_stocks = []
+    max_stocks = 7
 
     def strategy(self, event):
         # if not self.is_open:
         #     return
 
         self.make_min_cap_stocks(event)
+        self.rebalance(event)
 
         self.log.info(self.user.balance)
+        self.log.info(self.user.position)
+        self.log.info(self.user.entrust)
         self.log.info('\n')
 
 
@@ -43,19 +47,52 @@ class Strategy(StrategyTemplate):
         return DefaultLogHandler(self.name, log_type='file', filepath='log/%s-%s.log' % (self.name, today.isoformat()))
 
     def make_min_cap_stocks(self, event):
+        today = date.today()
+        if(self.last_sort_date and (today - self.last_sort_date).days > 15):
+            self.should_rebalance = True
+            return
+        else:
+            self.should_rebalance = False
+            self.last_sort_date = today
+
         eq = ts.Equity()
-        df = eq.Equ(equTypeCD='A', listStatusCD='L', field='ticker,secShortName,totalShares,nonrestFloatShares')
+        df = eq.Equ(equTypeCD='A', listStatusCD='L', field='ticker,secShortName,totalShares,nonrestFloatShares,listStatusCD')
         df['ticker'] = df['ticker'].map(lambda x: str(x).zfill(6))
+        df['listStatusCD'] = df['listStatusCD'].str.contains('L')
         tickers = df['ticker'].values
-        print(tickers)
+        # print(tickers)
 
         datas = [x for x in iter(event.data.values())]
         datas = list(filter(lambda x: True if x['code'] in tickers else False, datas))
         datas = list(filter(lambda x: x['总市值'] is not None and x['总市值'] > 0, datas))
         datas.sort(key=cmp_to_key(lambda x,y: x['总市值']-y['总市值']))
-        datas = datas[:7]
+        datas = datas[:self.max_stocks]
 
         self.min_cap_stocks = []
         for x in datas:
-            self.min_cap_stocks.append({'code':x['code'], '总市值':x['总市值']})
+            self.min_cap_stocks.append({'code':x['code'], '总市值':x['总市值'], 'name':x['name']})
         print(self.min_cap_stocks)
+
+    def rebalance(self, event):
+        if not self.should_rebalance:
+            return
+        else:
+            self.should_rebalance = False
+
+        positions = self.log.info(self.user.position)
+        position_codes = [x['stock_code'] for x in positions]
+        target_codes = [x['code'] for x in self.min_cap_stocks]
+
+        clear_codes = position_codes - target_codes
+        for x in clear_codes:
+            print('sell {}'.format(x))
+            self.user.sell(x)
+
+        balance = self.user.balance[0]
+        buy_codes = target_codes - position_codes
+        balance_each = balance / len(buy_codes)
+        for x in buy_codes:
+            bid1 = event.data[x].bid1
+            amount = int(balance_each / bid1 / 100) * 100
+            print('buy {} {}'.format(x, amount))
+            self.user.buy(x, bid1, amount)
